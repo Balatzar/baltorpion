7c2cd46f8f27bb06cf5b574274e2a54faa6ff8e1
blatalzar <bataprod@gmail.com>
2015-11-16 14:32:25 +0100

    save

--- scripts.js
+++ scripts.js
@@ -16,11 +16,11 @@
       for (var j = 0; j < gridSize; j++) {
         [-1, 0, 1].forEach(function(x) {
           [-1, 0, 1].forEach(function(y) {
-            var sum = parseInt(grid[i][j].className);
+            var sum = parseInt(grid[i][j].className[1]);
             if (grid[i + x] != undefined && grid[i + x][j + y] != undefined)
-            sum += parseInt(grid[i + x][j + y].className)
+            sum += parseInt(grid[i + x][j + y].className[1])
             if (grid[i + x + x] != undefined && grid[i + x + x][j + y + y] != undefined)
-              sum += parseInt(grid[i + x + x][j + y + y].className)
+              sum += parseInt(grid[i + x + x][j + y + y].className[1])
             if (sum == winCondition)
               return true;
             console.log(sum)
@@ -30,9 +30,9 @@
     }
   }
 
-  grid[0][0].className = "1";
-  grid[0][1].className = "1";
-  grid[0][2].className = "1";
+  grid[0][0].className = "a1";
+  grid[0][1].className = "a1";
+  grid[0][2].className = "a1";
 
   console.log(grid)
 
