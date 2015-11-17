7a5980cca912c92f2d4ebd9ef86a510d22bb659c
balatzar <bataprod@gmail.com>
2015-11-17 21:29:16 +0100

    fixed the js with better names and logic

--- scripts.js
+++ scripts.js
@@ -1,33 +1,30 @@
 (function () {
   var td = document.querySelectorAll("td");
-  var turn = true;
+  var turn = 0;
   var gridSize = Math.sqrt(td.length);
   var grid = [];
+  var winCondition = 3;
 
   for (var i = 0; i < td.length; i++) {
     td[i].addEventListener("click", function() {
       if (this.className)
         return;
-      if (turn) {
-        this.className = "a1";
-        turn = false;
-      } else {
-        this.className = "a9";
-        turn = true;
-      }
-      if (winGame())
-        if (turn)
-          endGame(1);
-        else
-          endGame(0);
+      this.className = turn % 2 == 0 ? "player1" : "player2";
+      if (winGame(this.className)) {
+          endGame(this.className);
+          return;
+        }
+      turn++;
+      if (turn == 9)
+        endGame();
     })
   }
 
   function endGame(player) {
     if (player)
-      alert("Joueur 2 gagne !");
+      alert(player + " gagne !");
     else
-      alert("Joueur 1 gagne !");
+      alert("Ex aequo !");
     location.reload(true);
   }
 
@@ -37,24 +34,27 @@
       grid[i].push(td[j + (gridSize * i)]);
   }
 
-  function winGame() {
+  function winGame(currentPlayer) {
     for (var i = 0; i < gridSize; i++) {
       for (var j = 0; j < gridSize; j++) {
         for (var x = -1; x < 2; x++) {
           for (var y = -1; y < 2; y++) {
-            var sum = parseInt(grid[i][j].className[1]);
-            if (grid[i + x] != undefined && grid[i + x][j + y] != undefined && (x != y || (x == 1 && y == 1))) {
-              sum += parseInt(grid[i + x][j + y].className[1]);
-            }
+            var points = 0
+            if (grid[i][j].className == currentPlayer)
+              points++
+            if (grid[i + x] != undefined && grid[i + x][j + y] != undefined && (x != y || (x == 1 && y == 1)))
+              if (grid[i + x][j + y].className == currentPlayer)
+                ++points;
             if (grid[i + x + x] != undefined && grid[i + x + x][j + y + y] != undefined && (x != y || (x == 1 && y == 1)))
-              sum += parseInt(grid[i + x + x][j + y + y].className[1]);
-            console.log(sum);
-            if (sum == 3 || sum == 27)
+              if (grid[i + x + x][j + y + y].className == currentPlayer)
+                ++points;
+            if (points == 3)
               return true;
           }
         }
       }
     }
+    return false;
   }
 
 })();
